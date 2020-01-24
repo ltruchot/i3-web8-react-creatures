eef02f691eac656fa2b5cfd776d310f390e61691
Loïc TRUCHOT <loic.truchot.pro@gmail.com>
2020-01-24 09:16:05 +0100

    chore(gitignore): build

---
+++ build/precache-manifest.aafb34dc52d31cbfebbeef70cc418dd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d909b53bb64fae9f3248ce4bddc0c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed53099b9f7f9815a782",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "25fca634b235bfc94bf9",
+    "url": "/static/js/2.a8258a4a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a8258a4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed53099b9f7f9815a782",
+    "url": "/static/js/main.3cf3afab.chunk.js"
+  },
+  {
+    "revision": "afe2ddb87a67b87b3fee",
+    "url": "/static/js/runtime-main.3b2870ab.js"
+  }
+]);
